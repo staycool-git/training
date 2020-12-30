2b28895da2c82bb666040cea92e80f01e752be12
staycool-git <janek.jarvpold@gmail.com>
2020-12-30 14:47:33 +0200

    Regulaaravaldis

--- demo.py
+++ demo.py
@@ -1,3 +1,17 @@
-print("Hello")
-print("Hello, hello")
-print("Welcome to you")
+import re
+info_text = """
+Toomas:
+    email: akadeemia-toomas@taltech.ee
+    phone: 5204 2456
+"""
+email_pattern = r'' # your pattern here
+phone_pattern = r'' # your pattern here
+
+email_match = re.search(email_pattern, info_text)
+phone_match = re.search(phone_pattern, info_text)
+
+email = email_match.group() if email_match else "NOT FOUND" # -> "akadeemia-toomas@taltech.ee"
+phone = phone_match.group() if phone_match else "NOT FOUND" # -> "5204 2456"
+
+print(email)
+print(phone)
