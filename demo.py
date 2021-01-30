a7763412f55c28ec3f8ceaa5052cd203031788b2
staycool-git <janek.jarvpold@gmail.com>
2021-01-30 16:35:46 +0200

    Regulaaravaldis

--- demo.py
+++ demo.py
@@ -1,17 +1,4 @@
-import re
-info_text = """
-Toomas:
-    email: akadeemia-toomas@taltech.ee
-    phone: 5204 2456
-"""
-email_pattern = r'' # your pattern here
-phone_pattern = r'' # your pattern here
+knights = {'gallahad': 'the pure', 'robin': 'the brave'}
 
-email_match = re.search(email_pattern, info_text)
-phone_match = re.search(phone_pattern, info_text)
-
-email = email_match.group() if email_match else "NOT FOUND" # -> "akadeemia-toomas@taltech.ee"
-phone = phone_match.group() if phone_match else "NOT FOUND" # -> "5204 2456"
-
-print(email)
-print(phone)
+for k, v in knights.items():
+    print(k, v)
